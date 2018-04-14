9fb7b7d2dd2abc5ea596b417c8c591ea88e7a867
nnian <35841252+orangecoi@users.noreply.github.com>
2018-04-14 21:36:11 +0200

    fix sync issues render chart ,add input range

--- config.js
+++ config.js
@@ -7,14 +7,14 @@ var refreshDelay = 30000;
 var networkStat = {
     "qwc": [
     	["qwertycoin.site", "https://qwertycoin.site:8119/stats"],
-		["pool.ahka.net/qwertycoin", "http://qwc.pool.ahka.net:8116/stats"],
-		["cryptoknight.cc/qwerty", "http://78.46.85.142:6559/stats"],
-		["qwerty.poolminer.work", "http://qwerty.poolminer.work:8117/stats"],
-		["pool.mineallcrypto.com", "http://pool.mineallcrypto.com:6076/stats"],
-		["superblockchain.con-ip.com", "http://superblockchain.con-ip.com:8333/stats"],
-		["qwertycoin.poolmining.it", "http://qwertycoin.poolmining.it:8117/stats"],
-		["qwc.dreampool.info", "https://qwc.dreampool.info/api/stats"],
 		["qwcpool.partyvibe.com","http://qwcpool.partyvibe.com:8118/stats"],
-		["qwertycoin.sixpools.tk", "http://qwertycoin.sixpools.tk:8118/stats"]		
+		["qwerty.mineallcrypto.com", "http://qwerty.mineallcrypto.com:6076/stats"],
+		["pool.ahka.net/qwertycoin", "http://qwc.pool.ahka.net:8116/stats"],
+        ["qwerty.poolminer.work", "http://qwerty.poolminer.work:8117/stats"],
+        ["wc.cnotepool.info", "http://qwc.cnotepool.info:8117/stats"],
+        ["cryptoknight.cc/qwerty", "http://78.46.85.142:6559/stats"],
+        ["superblockchain.con-ip.com", "http://superblockchain.con-ip.com:8333/stats"],
+        ["qwertycoin.poolmining.it", "http://qwertycoin.poolmining.it:8117/stats"],
+		["pool-marine.ga/qwc", "http://pool-marine.ga:8113/stats"]
     ]
 };
